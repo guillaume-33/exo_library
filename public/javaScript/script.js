9c4b460f515a734b8e18a6ace59bb6bd3176db9a
guillaume <laloum.guillaume@gmail.com>
2022-07-21 16:49:06 +0200

    ajout dark mode

--- public/javaScript/script.js
+++ public/javaScript/script.js
@@ -4,6 +4,7 @@ const dark = document.querySelector(".js_dark_mode"); // je dclare ma const via 
 dark.addEventListener('click', function (){
 
    const body = document.querySelector('.js_body'); //je selectionne le body via .js_body
+
    if(body.classList.contains('night')){ //si le body as deja la classe night,
       body.classList.remove('night')// je lui enleve
    }else {
